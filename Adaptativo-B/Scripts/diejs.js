26a2aa1e647dcfa43b9b9cb967cccd4a8af929c4
HybridWolf440 <al185636@alumnos.uacj.mx>
2025-08-05 22:17:48 -0600

    Add files via upload

---
+++ Adaptativo-B/Scripts/diejs.js
@@ -0,0 +1,8 @@
+var kelvin = 385;
+        var celsius = kelvin - 273;
+        var faherenheit_ = celsius * 1.8 + 32;
+
+        document.getElementById("fres").innerHTML = kelvin + " K equivale a " + faherenheit_ + "grados faherenheit";
+        document.getElementById("cres").innerHTML = kelvin + " K equivale a " + celsius + "grados Celsius";
+        
+        
